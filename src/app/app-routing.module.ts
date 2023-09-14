a9023b81fce76d1e4598d02a70dbe2d4dfcd104d
Rohit Kumar <rohit96682@gmail.com>
2023-09-14 22:53:35 +0530

    set all router and details component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { FundComponent } from './components/main/fund/fund.component';
 import { SchemeComponent } from './components/main/scheme/scheme.component';
 import { AddFundComponent } from './components/add-fund/add-fund.component';
+import { AddSchemeComponent } from './components/add-scheme/add-scheme.component';
+import { DetailsComponent } from './common-components/details/details.component';
 
 const routes: Routes = [
-  { path: 'fund', component: FundComponent },
+  { path: '', component: FundComponent },
   { path: 'scheme', component: SchemeComponent },
+  { path: 'scheme/add-scheme', component: AddSchemeComponent },
+  { path: 'scheme/add-scheme/details', component: DetailsComponent },
+
   { path: 'add-fund', component: AddFundComponent },
+  { path: 'add-fund/details', component: DetailsComponent },
 ];
 
 @NgModule({
