bec7f714b7dc901366a1c4ae21e17379a32f0471
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2023-09-14 16:55:31 +0530

    router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FundComponent } from './components/main/fund/fund.component';
+import { SchemeComponent } from './components/main/scheme/scheme.component';
+import { MainComponent } from './components/main/main.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent },
+  { path: 'fund', component: FundComponent },
+  { path: 'scheme', component: SchemeComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
