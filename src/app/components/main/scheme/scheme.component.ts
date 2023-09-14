d0180d99bcf730660f4b18709599c33b86ba5586
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2023-09-14 15:49:22 +0530

    scheme

--- src/app/components/main/scheme/scheme.component.ts
+++ src/app/components/main/scheme/scheme.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-scheme',
   templateUrl: './scheme.component.html',
-  styleUrls: ['./scheme.component.scss']
+  styleUrls: ['./scheme.component.scss'],
 })
 export class SchemeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+    ]),
+  });
+
+  onSubmit() {
+    console.log(this.loginForm.value);
   }
 
+  get emailValidator() {
+    return this.loginForm.get('email');
+  }
+
+  get passwordValidator() {
+    return this.loginForm.get('password');
+  }
 }
